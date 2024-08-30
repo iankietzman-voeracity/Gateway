957997dfc80f46f46e5d003b82819616689a7502
iankietzman-voeracity <ian+voeracity@tallgrassschool.com>
2024-08-30 16:36:14 -0500

    adds env var for deployment system IP

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function buildFastify() {
     reply.send({ hello: "world" });
   });
 
-  fastify.listen({ port: 8000, host: "0.0.0.0" }, function (err, _address) {
+  fastify.listen({ port: 8000, host: process.env.HOST_IP || "0.0.0.0" }, function (err, _address) {
     if (err) {
       fastify.log.error(err);
       process.exit(1);
