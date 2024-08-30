1140c3ee80622d3de021d76799557f54b7adea4a
iankietzman-voeracity <ian+voeracity@tallgrassschool.com>
2024-08-30 16:42:49 -0500

    prettier

--- index.js
+++ index.js
@@ -9,12 +9,15 @@ function buildFastify() {
     reply.send({ hello: "world" });
   });
 
-  fastify.listen({ port: 8000, host: process.env.HOST_IP || "0.0.0.0" }, function (err, _address) {
-    if (err) {
-      fastify.log.error(err);
-      process.exit(1);
-    }
-  });
+  fastify.listen(
+    { port: 8000, host: process.env.HOST_IP || "0.0.0.0" },
+    function (err, _address) {
+      if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+      }
+    },
+  );
 
   return fastify;
 }
